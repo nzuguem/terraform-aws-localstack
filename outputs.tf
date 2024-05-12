95b920515c2bdc09e944b38eea949b783909cee3
Kevin NZUGUEM <nzuguemkevine@gmail.com>
2024-05-12 20:20:05 +0000

    ♻️ Upgrade TF and use provider functions for AWS

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "test_secret_arn" {
   value = data.aws_secretsmanager_secret.test_secret.arn
 }
 
+output "test_secret_account_id" {
+  value = provider::aws::arn_parse(data.aws_secretsmanager_secret.test_secret.arn).account_id
+}
+
 output "users_arns" {
   value = local.users_arns[*].iam_user_arn
 }
